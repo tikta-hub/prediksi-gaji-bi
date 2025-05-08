9cf2fb374eefc286b59e07f4e28ff05e7575365e
tikta-hub <susilawatikta@gmail.com>
2025-05-08 09:52:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,6 @@ st.title("prediksi Gaji Berdasarkan Lama Bekerja")
 years_experience = st.number_input("Masukan jumlah tahun bekerja:", min_value=0.0, step=0.1)
 
 # Prediksi gaji
-if st.button("Prediksi Gaji")
+if st.button("Prediksi Gaji"):
   gaji = lin_reg_loaded.predict([[years_experience]])
   st.write(f"Gaji seseorang setelah bekerja selama {years_experience} tahun adalah ${gaji[0]:,.2f})
